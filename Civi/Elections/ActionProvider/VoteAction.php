62c01a4f0fe54bba46c1f2e0bbda8dc892f06804
Jaap Jansma <jaap.jansma@civicoop.org>
2020-06-09 21:30:32 +0200

    Integrated with form processor: Type for nominations and a Vote Action

---
+++ Civi/Elections/ActionProvider/VoteAction.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @author Jaap Jansma <jaap.jansma@civicoop.org>
+ * @license AGPL-3.0
+ */
+
+namespace Civi\Elections\ActionProvider;
+
+use Civi\ActionProvider\Action\AbstractAction;
+use Civi\ActionProvider\Exception\InvalidParameterException;
+use Civi\ActionProvider\Parameter\ParameterBagInterface;
+use Civi\ActionProvider\Parameter\Specification;
+use Civi\ActionProvider\Parameter\SpecificationBag;
+
+use CRM_Elections_ExtensionUtil as E;
+
+class VoteAction extends AbstractAction {
+
+  /**
+   * Run the action
+   *
+   * @param ParameterBagInterface $parameters
+   *   The parameters to this action.
+   * @param ParameterBagInterface $output
+   *   The parameters this action can send back
+   *
+   * @return void
+   */
+  protected function doAction(ParameterBagInterface $parameters, ParameterBagInterface $output) {
+    $voter_contact_id = $parameters->getParameter('voter_contact_id');
+    $election_nomination_id = $parameters->getParameter('nomination_id');
+    $rank = 1;
+    if ($parameters->doesParameterExists('rank')) {
+      $rank = $parameters->getParameter('rank');
+    }
+
+    $vote['version'] = 3;
+    $vote['member_id'] = $voter_contact_id;
+    $vote['election_nomination_id'] = $election_nomination_id;
+    $vote['rank'] = $rank;
+    civicrm_api('ElectionVote', 'create', $vote);
+  }
+
+  /**
+   * @return bool
+   * @throws \Civi\ActionProvider\Exception\InvalidParameterException
+   */
+  protected function validateParameters(ParameterBagInterface $parameters) {
+    $return = parent::validateParameters($parameters);
+    $election_id = $this->configuration->getParameter('election_id');
+    $voter_contact_id = $parameters->getParameter('voter_contact_id');
+    $election = findElectionById($election_id);
+    if (!$election->isVisible) {
+      throw new InvalidParameterException('Voting is closed.');
+    }
+    if (!$election->isVotingStarted) {
+      throw new InvalidParameterException('Voting is closed.');
+    }
+    if ($election->isVotingEnded) {
+      throw new InvalidParameterException('Voting is closed.');
+    }
+    if (hasLoggedInUserAlreadyVoted($election_id, $voter_contact_id)) {
+      throw new InvalidParameterException('User has already voted.');
+    }
+    return $return;
+  }
+
+
+  /**
+   * Returns the specification of the configuration options for the actual
+   * action.
+   *
+   * @return SpecificationBag
+   */
+  public function getConfigurationSpecification() {
+    $elections = [];
+    $electionsApi = civicrm_api3('Election', 'get', [
+      'is_visible' => 1,
+      'options' => ['limit' => 0],
+    ]);
+    foreach ($electionsApi['values'] as $election) {
+      $elections[$election['id']] = $election['name'];
+    }
+
+    return new SpecificationBag([
+      new Specification('election_id', 'Integer', E::ts('Election ID'), true, null, null, $elections),
+    ]);
+  }
+
+  /**
+   * Returns the specification of the parameters of the actual action.
+   *
+   * @return SpecificationBag
+   */
+  public function getParameterSpecification() {
+    return new SpecificationBag([
+      new Specification('voter_contact_id', 'Integer', E::ts('Voter Contact ID'), true),
+      new Specification('nomination_id', 'Integer', E::ts('Nomination ID'), true),
+      new Specification('rank', 'Integer', E::ts('Rank')),
+    ]);
+  }
+
+}
---
+++ Civi/Elections/FormProcessor/NominationType.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @author Jaap Jansma <jaap.jansma@civicoop.org>
+ * @license AGPL-3.0
+ */
+
+namespace Civi\Elections\FormProcessor;
+
+use Civi\FormProcessor\Config\Specification;
+use Civi\FormProcessor\Config\SpecificationBag;
+use Civi\FormProcessor\Type\AbstractType;
+use Civi\FormProcessor\Type\OptionListInterface;
+
+use CRM_Elections_ExtensionUtil as E;
+
+class NominationType extends AbstractType implements OptionListInterface {
+
+  public function __construct() {
+    parent::__construct('election_nomination', E::ts('Nomination'));
+  }
+
+
+  /**
+   * Get the configuration specification
+   *
+   * @return SpecificationBag
+   */
+  public function getConfigurationSpecification() {
+    $positions = [];
+    $electionsApi = civicrm_api3('Election', 'get', [
+      'is_visible' => 1,
+      'options' => ['limit' => 0],
+    ]);
+    foreach ($electionsApi['values'] as $election) {
+      $positionsApi = civicrm_api3('ElectionPosition', 'get', [
+        'election_id' => $election['id'],
+        'options' => ['limit' => 0],
+      ]);
+      foreach ($positionsApi['values'] as $position) {
+        $positions[$position['id']] = $election['name']. ': '.$position['name'];
+      }
+    }
+
+    return new SpecificationBag([
+      new Specification('position_id', 'Integer', E::ts('Position'), TRUE, NULL, NULL, $positions, FALSE),
+    ]);
+  }
+
+
+  /**
+   * Returns whether the provided value is a valid
+   *
+   * @param mixed $value
+   * @param array $allValues
+   *
+   * @return bool
+   */
+  public function validateValue($value, $allValues = []) {
+    $options = $this->getOptions($allValues);
+    if (isset($options[$value])) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Returns the type number from CRM_Utils_Type
+   */
+  public function getCrmType() {
+    return \CRM_Utils_Type::T_INT;
+  }
+
+  /**
+   * Returns an array with the options.
+   *
+   * The key of the array is the value and the array item is the label.
+   *
+   * @param array $params
+   *   This array contains the params used to call getFields. This way we could
+   *   build conditional option lists.
+   *
+   * @return array
+   */
+  public function getOptions($params) {
+    $position_id = $this->configuration->get('position_id');
+    $nominationsApi = civicrm_api3('ElectionNomination', 'get', [
+      'election_position_id' => $position_id,
+      'has_accepted_nomination' => 1,
+      'is_eligible_candidate' => 1,
+    ]);
+    $nominations = [];
+    foreach($nominationsApi['values'] as $nomination) {
+      $display_name = $nomination['id'];
+      try {
+        $display_name = civicrm_api3('Contact', 'getvalue', [
+          'id' => $nomination['member_nominee'],
+          'return' => 'display_name'
+        ]);
+      } catch (\CiviCRM_API3_Exception $ex) {
+        // Do nothing.
+      }
+      $nominations[$nomination['id']] = $display_name;
+    }
+    return $nominations;
+  }
+
+  /**
+   * Returns true when this field is a multiple field.
+   *
+   * @return bool
+   */
+  public function isMultiple() {
+    return FALSE;
+  }
+
+
+}
