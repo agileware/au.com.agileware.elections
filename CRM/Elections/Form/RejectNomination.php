4a1ba465edf9b4f36f9c25c131a4feb91c67cc83
Iris Abarquez <iris@agileware.com.au>
2025-10-21 11:12:51 +1100

    CIVIELECT-13 Support checksum validated anonymous user to Withdraw Nomination

--- CRM/Elections/Form/RejectNomination.php
+++ CRM/Elections/Form/RejectNomination.php
@@ -9,6 +9,8 @@ use CRM_Elections_ExtensionUtil as E;
  */
 class CRM_Elections_Form_RejectNomination extends CRM_Elections_Form_Base {
   private $enId = 0;
+  private $cid = NULL;
+  private $cs = NULL;
   private $electionNomination = NULL;
 
   public function buildQuickForm() {
@@ -54,6 +56,21 @@ class CRM_Elections_Form_RejectNomination extends CRM_Elections_Form_Base {
     $this->add('textarea', 'nominationcomments', 'Comments', ['cols' => 55, 'rows' => 6], FALSE);
     $this->addElement('hidden', 'enid', $this->enId);
 
+    if ( $this->cid && $this->cs ) {
+      // Expose to Smarty
+      $contact = \Civi\Api4\Contact::get(FALSE)
+                                    ->addSelect('email_primary', 'display_name')
+                                    ->addWhere('id', '=', $this->cid)
+                                    ->execute()
+                                    ->first();
+
+      $this->assign( 'checksum_authenticated', $contact );
+
+      // Add to form elements
+      $this->addElement('hidden', 'cid', $this->cid);
+      $this->addElement('hidden', 'cs', $this->cs);
+    }
+
     $this->addButtons([
       [
         'type' => 'submit',
@@ -63,6 +80,18 @@ class CRM_Elections_Form_RejectNomination extends CRM_Elections_Form_Base {
     ]);
 
     parent::buildQuickForm();
+  }
+
+  public function preProcess() {
+    $cid = CRM_Utils_Request::retrieve('cid', 'Positive');
+    $cs = CRM_Utils_Request::retrieve('cs', 'String');
+
+    // Only store these if the user is not logged in. Otherwise we want to
+    // defer to the logged in contact.
+    if ( empty( \CRM_Core_Session::getLoggedInContactID() ) && $cid && $cs ) {
+      $this->cid = $cid;
+      $this->cs = $cs;
+    }
   }
 
   public function postProcess() {
@@ -76,7 +105,17 @@ class CRM_Elections_Form_RejectNomination extends CRM_Elections_Form_Base {
     ]);
 
     CRM_Core_Session::setStatus('You have withdrawn the nomination.', '', 'success');
-    CRM_Utils_System::redirect(Civi::url('current://civicrm/elections/view', 'eid=' . $this->electionNomination['election_position_id.election_id'] ));
+
+    // Redirect back to the main election info view
+    $redirectUrl = Civi::url('current://civicrm/elections/view');
+    $redirectUrl->addQuery(['eid' => $this->electionNomination['election_position_id.election_id']]);
+
+    // Conditionally add contact ID and checksum
+    if ( $this->cid && $this->cs ) {
+        $redirectUrl->addQuery(['cid' => $this->cid]);
+        $redirectUrl->addQuery(['cs' => $this->cs]);
+    }
+    CRM_Utils_System::redirect( $redirectUrl );
 
     parent::postProcess();
   }
