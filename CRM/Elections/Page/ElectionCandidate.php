e74f728d878b65d44473e9883409da2eb04e34f0
Iris Abarquez <iris@agileware.com.au>
2025-10-22 10:47:57 +1100

    CIVIELECT-13 Support checksum validated anonymous user to view candidate profiles

--- CRM/Elections/Page/ElectionCandidate.php
+++ CRM/Elections/Page/ElectionCandidate.php
@@ -45,6 +45,42 @@ class CRM_Elections_Page_ElectionCandidate extends CRM_Elections_Page_Base {
     $this->assign('election', $this->election);
     CRM_Utils_System::setTitle($this->electionNomination['member_nominee.display_name'] . ' - ' . $this->election->name);
 
+    $this->assign('checksum_query', [
+      'query' => sprintf( 'eid=%s', $this->electionNomination['election_position_id.election_id'] )
+    ]);
+
+    // Support anonymous validated via contact checksum
+    // Only validate cs and cid if the user is not logged in
+    if ( empty( \CRM_Core_Session::getLoggedInContactID() ) ) {
+      // The custom access callback should already validate this, but
+      // we want the utils function to always validate anyway, just
+      // in case it is used outside of a page with the access
+      // callback for whatever reason.
+      $contact_valid_checksum = retrieveContactChecksumFromUrl( $this );
+
+      if ( $contact_valid_checksum ) {
+        // Build a query string including the election ID and validated checksum
+        $this->assign('checksum_query', [
+          'cid' => $contact_valid_checksum['cid'],
+          'cs' => $contact_valid_checksum['cs'],
+          'query' => sprintf( 'eid=%s&cs=%s&cid=%s', $this->electionNomination['election_position_id.election_id'], $contact_valid_checksum['cs'], $contact_valid_checksum['cid'] )
+        ]);
+
+        // Expose to Smarty
+        $contact = \Civi\Api4\Contact::get(FALSE)
+                                    ->addSelect('email_primary', 'display_name')
+                                    ->addWhere('id', '=', $contact_valid_checksum['cid'])
+                                    ->execute()
+                                    ->first();
+
+        $this->assign( 'checksum_authenticated', $contact );
+        $this->assign( 'loggedInContactId', $contact_valid_checksum['cid'] ); // Override
+
+        $login_url = getLoginPageURL(\CRM_Utils_System::currentPath());
+        $this->assign( 'login_url', sprintf( '%s?eid=%s', $login_url, $this->electionNomination['election_position_id.election_id'] ) );
+      }
+    }
+
     parent::run();
   }
 
