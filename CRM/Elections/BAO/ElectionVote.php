019fd1fea4d9ee0f9e96b1e7e6d5009cbc224470
colemanw <coleman@civicrm.org>
2025-10-23 15:08:47 -0400

    Remove commented-out boilerplate
    Civix used to generate commented-out BAO create functions, however all BAOs now inherit writeRecords() from their parent.
    
    Since this function was never uncommented, it's safe to remove.

--- CRM/Elections/BAO/ElectionVote.php
+++ CRM/Elections/BAO/ElectionVote.php
@@ -3,24 +3,5 @@ use CRM_Elections_ExtensionUtil as E;
 
 class CRM_Elections_BAO_ElectionVote extends CRM_Elections_DAO_ElectionVote {
 
-  /**
-   * Create a new ElectionVote based on array-data
-   *
-   * @param array $params key-value pairs
-   * @return CRM_Elections_DAO_ElectionVote|NULL
-   *
-  public static function create($params) {
-    $className = 'CRM_Elections_DAO_ElectionVote';
-    $entityName = 'ElectionVote';
-    $hook = empty($params['id']) ? 'create' : 'edit';
-
-    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
-    $instance->copyValues($params);
-    $instance->save();
-    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
-
-    return $instance;
-  } */
 
 }
